5dc799733d8fb55c2161d0a6075c93f8c77c59c4
jiasong <593908937@qq.com>
2020-08-05 17:03:42 +0800

    优化 spliceURLString

--- JSNetwork/JSNetworkUtil.m
+++ JSNetwork/JSNetworkUtil.m
@@ -19,14 +19,14 @@
 @implementation JSNetworkUtil (URL)
 
 + (NSString *)spliceURLString:(NSString *)URLString withParameter:(NSDictionary *)parameter {
-    NSString *newUrl = [NSString stringWithString:URLString];
-    NSString *newQuery = @"";
-    if ([URLString containsString:@"?"]) {
-        NSArray *array = [URLString componentsSeparatedByString:@"?"];
-        newUrl = array.firstObject;
-        newQuery = array.lastObject;
-    }
-    NSMutableDictionary *newParameters = [NSMutableDictionary dictionaryWithDictionary:[NSDictionary js_dictionaryQueryWithURLString:newQuery]];
+    NSURLComponents *components = [NSURLComponents componentsWithString:[URLString stringByAddingPercentEncodingWithAllowedCharacters:NSCharacterSet.URLQueryAllowedCharacterSet]];
+    NSString *scheme = components.scheme ? [NSString stringWithFormat:@"%@://", components.scheme] : @"";
+    NSString *host = components.host ? : @"";
+    NSString *port = components.port ? [NSString stringWithFormat:@":%@", components.port] : @"";
+    NSString *path = components.path ? : @"";
+    NSString *query = components.query ? : @"";
+    NSString *newUrl = [NSString stringWithFormat:@"%@%@%@%@", scheme, host, port, path];
+    NSMutableDictionary *newParameters = [NSMutableDictionary dictionaryWithDictionary:[NSDictionary js_dictionaryQueryWithURLString:query]];
     [newParameters addEntriesFromDictionary:parameter];
     if (newParameters.count > 0) {
         newUrl = [newUrl stringByAppendingFormat:@"?%@", newParameters.js_URLQueryString];
