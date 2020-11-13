92c9bbede6e7f7e1400bcbb82433a14c23b305dc
jiasong <593908937@qq.com>
2020-11-13 17:40:55 +0800

    支持二进制数据

--- Example/JSNetworkExample/Services/Config/NetworkRequest.m
+++ Example/JSNetworkExample/Services/Config/NetworkRequest.m
@@ -17,21 +17,20 @@
     NSURLSessionTask *_requestTask;
 }
 
-@property (nonatomic, copy) id taskCompleted;
-@property (nonatomic, strong) id config;
-
 @end
 
 @implementation NetworkRequest
 
 - (void)buildTaskWithRequestConfig:(id<JSNetworkRequestConfigProtocol>)config taskCompleted:(void (^)(id _Nullable, NSError * _Nullable))taskCompleted {
     [super buildTaskWithRequestConfig:config taskCompleted:taskCompleted];
-    _taskCompleted = taskCompleted;
-    _config = config;
-    AFHTTPSessionManager *manger = [AFHTTPSessionManager manager];
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     BOOL useFormData = false;
     AFHTTPRequestSerializer *requestSerializer = [AFJSONRequestSerializer serializer];
     switch (config.requestSerializerType) {
+        case JSRequestSerializerTypeHTTP:
+        case JSRequestSerializerTypeBinaryData:
+            requestSerializer = [AFHTTPRequestSerializer serializer];
+            break;
         case JSRequestSerializerTypeFormData:
             useFormData = true;
             requestSerializer = [AFHTTPRequestSerializer serializer];
@@ -55,25 +54,26 @@
         [requestSerializer setValue:headers[headerField] forHTTPHeaderField:headerField];
     }
     requestSerializer.timeoutInterval = config.requestTimeoutInterval;
-    manger.completionQueue = JSNetworkConfig.sharedConfig.processingQueue;
-    manger.requestSerializer = requestSerializer;
-    manger.responseSerializer = responseSerializer;
-    manger.responseSerializer.acceptableStatusCodes = config.responseAcceptableStatusCodes;
+    manager.completionQueue = JSNetworkConfig.sharedConfig.processingQueue;
+    manager.requestSerializer = requestSerializer;
+    manager.responseSerializer = responseSerializer;
+    manager.responseSerializer.acceptableStatusCodes = config.responseAcceptableStatusCodes;
     if (config.responseAcceptableContentTypes) {
-        NSMutableSet *contentTypes = [NSMutableSet setWithSet:manger.responseSerializer.acceptableContentTypes];
+        NSMutableSet *contentTypes = [NSMutableSet setWithSet:manager.responseSerializer.acceptableContentTypes];
         [contentTypes unionSet:config.responseAcceptableContentTypes];
-        manger.responseSerializer.acceptableContentTypes = contentTypes.copy;
+        manager.responseSerializer.acceptableContentTypes = contentTypes.copy;
     }
-    __weak __typeof(manger) weakManger = manger;
+    __weak __typeof(manager) weakManager = manager;
     void (^completed)(id, NSError *) = ^(id responseObject, NSError *error) {
         taskCompleted(responseObject, error);
-        [weakManger invalidateSessionCancelingTasks:false resetSession:false];
+        [weakManager invalidateSessionCancelingTasks:false resetSession:false];
     };
     if (useFormData) {
-        _requestTask = [manger POST:config.requestUrl
-                         parameters:config.requestBody
-                            headers:nil
-          constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+        _requestTask = [manager
+                        POST:config.requestUrl
+                        parameters:config.requestBody
+                        headers:nil
+                        constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
             if ([config respondsToSelector:@selector(constructingMultipartFormData:)]) {
                 [config constructingMultipartFormData:formData];
             }
@@ -81,30 +81,50 @@
             if (self.interfaceProxy.uploadProgress) {
                 self.interfaceProxy.uploadProgress(uploadProgress);
             }
-        } success:^(NSURLSessionDataTask *task, id responseObject) {
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
             completed(responseObject, nil);
-        } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
             completed(nil, error);
         }];
     } else {
-        NSString *HTTPMethod = config.requestMethod == JSRequestMethodGET ? @"GET" : @"POST";
-        _requestTask = [manger dataTaskWithHTTPMethod:HTTPMethod
-                                            URLString:config.requestUrl
-                                           parameters:config.requestBody
-                                              headers:nil
-                                       uploadProgress:^(NSProgress *uploadProgress) {
-            if (self.interfaceProxy.uploadProgress) {
-                self.interfaceProxy.uploadProgress(uploadProgress);
+        NSString *method = @"";
+        switch (config.requestMethod) {
+            case JSRequestMethodGET:
+                method = @"GET";
+                break;
+            case JSRequestMethodPOST:
+                method = @"POST";
+                break;
+            default:
+                break;
+        }
+        id requestBody = config.requestBody;
+        BOOL isBinaryBody = config.requestSerializerType == JSRequestSerializerTypeBinaryData && [requestBody isKindOfClass:NSData.class];
+        NSError *serializationError = nil;
+        NSMutableURLRequest *request = [manager.requestSerializer requestWithMethod:method
+                                                                          URLString:[[NSURL URLWithString:config.requestUrl relativeToURL:manager.baseURL] absoluteString]
+                                                                         parameters:isBinaryBody ? nil : requestBody
+                                                                              error:&serializationError];
+        if (serializationError) {
+            completed(nil, serializationError);
+        } else {
+            if (isBinaryBody) {
+                [request setHTTPBody:requestBody];
             }
-        } downloadProgress:^(NSProgress *downloadProgress) {
-            if (self.interfaceProxy.downloadProgress) {
-                self.interfaceProxy.downloadProgress(downloadProgress);
-            }
-        } success:^(NSURLSessionDataTask *task, id responseObject) {
-            completed(responseObject, nil);
-        } failure:^(NSURLSessionDataTask *task, NSError *error) {
-            completed(nil, error);
-        }];
+            _requestTask = [manager dataTaskWithRequest:request
+                                         uploadProgress:^(NSProgress * _Nonnull uploadProgress) {
+                if (self.interfaceProxy.uploadProgress) {
+                    self.interfaceProxy.uploadProgress(uploadProgress);
+                }
+            } downloadProgress:^(NSProgress * _Nonnull downloadProgress) {
+                if (self.interfaceProxy.downloadProgress) {
+                    self.interfaceProxy.downloadProgress(downloadProgress);
+                }
+            } completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+                completed(responseObject, error);
+            }];
+            [_requestTask resume];
+        }
     }
 }
 
