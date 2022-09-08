b3902f051db2ae206ca855c96bcb6c2675c09102
jiasong <593908937@qq.com>
2022-09-08 13:36:46 +0800

    不值一提的commit

--- Example/JSNetworkExample/AlamofireRequest1.swift
+++ Example/JSNetworkExample/AlamofireRequest1.swift
@@ -11,72 +11,82 @@ import JSNetwork
 
 @objc open class AlamofireRequest1: JSNetworkRequest {
     
-    private var dataRequest: DataRequest!
-    private var task: URLSessionTask!
+    private var dataRequest: DataRequest?
+    private var task: URLSessionTask?
     
-//    open override func buildTask(withConfig config: JSNetworkRequestConfigProtocol,
-//                                 multipartFormData multipartFormDataBlock: @escaping (Any) -> Void,
-//                                 uploadProgress uploadProgressBlock: @escaping (Progress) -> Void,
-//                                 downloadProgress downloadProgressBlock: @escaping (Progress) -> Void,
-//                                 didCreateURLRequest didCreateURLRequestBlock: @escaping (NSMutableURLRequest) -> Void,
-//                                 didCreateTask didCreateTaskBlock: @escaping (URLSessionTask) -> Void,
-//                                 didCompleted didCompletedBlock: @escaping (Any?, Error?) -> Void) {
-//        guard let url: URL = URL(string: config.requestUrlString()) else {
-//            let error: NSError = NSError(domain: "com.alamofire.error", code: 404, userInfo: nil)
-//            return didCompletedBlock(nil, error)
-//        }
-//        var method: HTTPMethod = .get
-//        switch config.requestMethod?() {
-//        case .get:
-//            method = .get
-//            break
-//        case .post:
-//            method = .post
-//            break
-//        default:
-//            break
-//        }
-//        let requestBody: Dictionary<String, Any>? = config.requestBody?() as? Dictionary
-//        let responseSerializer = self.buildResponseSerializer(with: config)
-//        let monitor = ClosureEventMonitor()
-//        monitor.requestDidCreateURLRequest = { (requst: Request, urlRequest: URLRequest) in
-//            let mutableURLRequest = urlRequest as? NSMutableURLRequest ?? NSMutableURLRequest(url: url)
-//            didCreateURLRequestBlock(mutableURLRequest)
-//        }
-//        monitor.requestDidCreateTask = { [weak self](requst: Request, task: URLSessionTask) in
-//            self?.task = task
-//            didCreateTaskBlock(task)
-//        }
-//        let session = Session(startRequestsImmediately: false, eventMonitors: [monitor])
-//        self.dataRequest = session.request(url)
-//    }
+    open override func buildTask(withConfig config: JSNetworkRequestConfigProtocol,
+                                 uploadProgress: @escaping (Progress) -> Void,
+                                 downloadProgress: @escaping (Progress) -> Void,
+                                 constructingFormData: @escaping (Any) -> Void,
+                                 didCreateURLRequest: @escaping (URLRequest) -> URLRequest,
+                                 didCreateTask: @escaping (URLSessionTask) -> URLSessionTask,
+                                 didCompleted: @escaping (Any?, Error?) -> Void) {
+        guard let url: URL = URL(string: config.requestUrlString()) else {
+            let error: NSError = NSError(domain: "com.alamofire.error", code: 404, userInfo: nil)
+            return didCompleted(nil, error)
+        }
+        var method: HTTPMethod = .get
+        switch config.requestMethod?() {
+        case .get:
+            method = .get
+            break
+        case .post:
+            method = .post
+            break
+        case .head:
+            method = .head
+            break
+        case .put:
+            method = .put
+            break
+        case .delete:
+            method = .delete
+            break
+        case .patch:
+            method = .patch
+            break
+        default:
+            break
+        }
+        let requestBody = config.requestBody?()
+        var responseSerializer =
+        let type: JSResponseSerializerType = config.responseSerializerType?() ?? .json
+        if type == .http {
+            responseSerializer =  StringResponseSerializer()
+        }
+        responseSerializer = JSONResponseSerializer()
+        let redirector = Redirector(behavior: .modify({ (task, request, response) -> URLRequest in
+            return didCreateURLRequest(request)
+        }))
+        let monitor = ClosureEventMonitor()
+        monitor.requestDidCreateTask = { [weak self](requst: Request, task: URLSessionTask) in
+            self?.task = didCreateTask(task)
+        }
+        let session = Session(startRequestsImmediately: false, redirectHandler: redirector, eventMonitors: [monitor])
+        self.dataRequest = session.request(url,
+                                           method: method,
+                                           parameters: nil,
+                                           headers: nil,
+                                           interceptor: nil,
+                                           requestModifier: nil)
+        self.dataRequest?.response(responseSerializer: responseSerializer, completionHandler: { responseObject in
+            
+        })
+    }
     
     open override func requestTask() -> URLSessionTask {
-        return self.task
+        guard let task = self.task else {
+            fatalError("不能为nil")
+        }
+        return task
     }
     
     override open func start() -> Void {
-        self.dataRequest.resume()
+        self.dataRequest?.resume()
     }
     
     override open func cancel() -> Void {
-        self.dataRequest.cancel()
-    }
-    
-    deinit {
-        
-    }
-    
-}
-
-extension AlamofireRequest1 {
-    
-    func buildResponseSerializer(with config: JSNetworkRequestConfigProtocol) -> Any {
-        let type: JSResponseSerializerType = config.responseSerializerType?() ?? .json
-        if type == .http {
-            return StringResponseSerializer()
-        }
-        return JSONResponseSerializer()
+        self.dataRequest?.cancel()
     }
     
 }
